a39c9672e51b2ef77e2edad77cee0ac909c1a571
Vsevolod Vaskin <vaskin.2003@gmail.com>
2023-08-12 10:42:08 +0000

    base solution implemented

--- sine/sine.s
+++ sine/sine.s
@@ -16,13 +16,23 @@ var:
 #	a1 -- input buffer will contain string with the argument
 #	a2 -- output string buffer for the string result
 sine:
-	# implement here
+	# load first byte
+	lb t0, 0(a1)
+	sb t0, 0(a2)
 
-	la	a7, var
+	# if first symbol is '0', then answer is 1 and program is finished
+	li t6, '1'
+	beq t0, t6, finish
 	
-	li	a3, default_answer
-	sw	a3, 0(a2)
-	sw	a3, 0(a7)
+	# else copy input to output
+copy_loop:
+	addi a1, a1, 1
+	addi a2, a2, 1
+	lb t0, 0(a1)
+	sb t0, 0(a2)
+	bne t0, zero, copy_loop
 
+
+finish:
 	ret
-	
+
